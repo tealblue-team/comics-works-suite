5afd0555d6d0aa30dc836386c9b020b6b093ce1e
Marco Piccolino <marco.a.piccolino@gmail.com>
2018-07-18 07:53:34 +0200

    utils - add random seed

--- comics/works/utils/utils.cpp
+++ comics/works/utils/utils.cpp
@@ -6,10 +6,11 @@
 comics::works::Utils::Utils(QObject* parent) : QObject(parent) {}
 
 QString comics::works::Utils::generateRandomId(int size) {
+    std::srand(std::time(0));
     std::array<char,26> abc({{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}});
     QString randomId = "";
     for (int i=0;i<size;++i) {
-        randomId += QString(abc[rand()/((RAND_MAX + 1u)/26)]);
+        randomId += QString(abc[std::rand()/((RAND_MAX + 1u)/26)]);
     }
     return randomId.toUpper();
 }
